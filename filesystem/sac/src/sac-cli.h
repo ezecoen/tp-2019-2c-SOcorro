59633b806d1162627fbcf7da7c9a391c6d9d4e10
Juan Mesaglio <juan.mesaglio@gmail.com>
2019-10-02 20:26:04 -0300

    nose que wea

---
+++ filesystem/sac/src/sac-cli.h
@@ -0,0 +1,17 @@
+#ifndef SAC_CLI_H_
+#define SAC_CLI_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <commons/string.h>
+#include <sys/statfs.h>
+
+
+
+#endif /* SAC_CLI_H_ */
