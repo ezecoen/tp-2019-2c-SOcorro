79bd1294ea26f30a82910d90fea970e829607169
achani2497 <alejandro.chani24@gmail.com>
2019-11-22 12:53:40 -0300

    Programa de prueba para suse

---
+++ planificador/pruebitaSuse/src/pruebita.h
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ucontext.h>
+#include <unistd.h>
+#include <hilolay/hilolay.h>
+
+void recursiva();
+void *test1();
+void *test2();
